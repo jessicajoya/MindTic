3cfc40ae3f7e7962b6e503b809af13fbc2d4339f
julianud4 <alfonsoud4@yahoo.es>
2022-08-22 20:46:24 -0500

    modelo base del proyecto MindTic
    Se realiza la creacion del modelo basico para el proyecto del ciclo 3 para el curso de programacion Mintic

---
+++ src/main/java/com/mindTic/MindTic/MindTicApplication.java
@@ -0,0 +1,13 @@
+package com.mindTic.MindTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindTicApplication.class, args);
+	}
+
+}
